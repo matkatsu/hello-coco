0ede626c1cd64725e0d58304c68a3de71caaafb1
romiogaku <romiogaku@me.com>
2018-12-30 22:23:53 +0900

    歌うよ

---
+++ constants.js
@@ -0,0 +1,12 @@
+exports.audioData = [
+  {
+    title: "いっしょにA･I･K･A･T･S･U！",
+    url:
+      "https://s3-ap-northeast-1.amazonaws.com/romiogaku/alexa/hello-koko/music/%E3%81%84%E3%81%A3%E3%81%97%E3%82%87%E3%81%ABA%EF%BD%A5I%EF%BD%A5K%EF%BD%A5A%EF%BD%A5T%EF%BD%A5S%EF%BD%A5U%EF%BC%81.mp3"
+  }
+];
+
+exports.voiceData = {
+  kokodayo:
+    "https://s3-ap-northeast-1.amazonaws.com/romiogaku/alexa/hello-koko/sounds/kokodayo.mp3"
+};
